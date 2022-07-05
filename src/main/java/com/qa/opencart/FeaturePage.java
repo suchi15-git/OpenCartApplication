5930d81f41405205bffe02c11049f86aa3a64996
suchita-kadge <suchitakadge35@gmail.com>
2022-07-05 11:48:56 +0530

    f1 version1

--- src/main/java/com/qa/opencart/FeaturePage.java
+++ src/main/java/com/qa/opencart/FeaturePage.java
@@ -3,7 +3,7 @@ package com.qa.opencart;
 public class FeaturePage {
 	
 	public static void main (String [] args) {
-		System.out.println("added for fun");
+		System.out.println("added for fun1");
 	}
 
 }
